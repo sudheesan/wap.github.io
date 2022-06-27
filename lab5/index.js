a4aac32f7c5d23aeefe4311ab5314586e2c67833
Sudheesan <surendransudheesan@gmail.com>
2022-06-27 13:35:07 -0500

    updated
    updated

--- lab5/index.js
+++ lab5/index.js
@@ -1,12 +1,7 @@
 
-const sum = (numArray) => {
-    return numArray.filter(value => value > 20)
-    .reduce((a,b)=> a+b)
-}
+const sum = (numArray) => numArray.filter(value => value > 20).reduce((a,b)=> a+b)
 
-const numArray = (stringArray) => {
-    return stringArray.filter((value) => value.length >= 5 && value.includes('a'));
-}
+const numArray = (stringArray) =>  stringArray.filter((value) => value.length >= 5 && value.includes('a'));
 
 //test array question one
 const newNumArray = [1,25,3,25,5];
