b0082045313340860d7608ad30bb8eb808d76dcd
Sudheesan <surendransudheesan@gmail.com>
2022-06-29 19:09:39 -0500

    Minor Update
    Minor Update

--- lab8/index.js
+++ lab8/index.js
@@ -1,4 +1,7 @@
 //---Question1---//
+
+console.log("//---Question1---//")
+
 const studentArray = [];
 const student = {
     firstName: 'Sudheesan',
@@ -21,16 +24,19 @@ for (let i = 0; i < 5; i++) {
     studentArray.push(newStudent)
 }
 
-// console.log(studentArray);
+console.log(studentArray);
 
-// console.log("The Average : ", studentArray.reduce((prev, curr) => prev + (curr.compusteAverageGrade() / studentArray.length), 0));
+console.log("The Average : ", studentArray.reduce((prev, curr) => prev + (curr.compusteAverageGrade() / studentArray.length), 0));
 
-
+console.log("//---Question1---//", '\n', '\n', '\n')
 //---Question1---//
 
 
 
 //---Question2---//
+
+console.log("//---Question2---//")
+
 
 const studentArray2 = [];
 
@@ -56,15 +62,19 @@ for (let i = 0; i < 5; i++) {
     studentArray2.push(newStudent)
 }
 
-//console.log(studentArray2);
+console.log(studentArray2);
 
-// console.log("The Average : ", studentArray2.reduce((prev, curr) => prev + (curr.compusteAverageGrade() / studentArray2.length), 0));
+console.log("The Average : ", studentArray2.reduce((prev, curr) => prev + (curr.compusteAverageGrade() / studentArray2.length), 0));
 
-//---Question2---//
-
-
+console.log("//---Question2---//", '\n', '\n', '\n')
 
 //---Question2---//
+
+
+
+//---Question4---//
+
+console.log("//---Question4---//")
 
 const LinkedList = function () {
     this.list = [];
@@ -85,9 +95,8 @@ LinkedList.prototype.remove = function (value) {
 }
 
 LinkedList.prototype.print = function () {
-    console.log("LinkedList", this.list);
+    console.log("LinkedList", `{${this.list.toString()}}`);
 }
-
 
 let linkedlist = new LinkedList();
 linkedlist.add(1);
@@ -97,7 +106,6 @@ linkedlist.print(); //Expected Result: LinkedList{1,2,3};
 linkedlist.remove(2);
 linkedlist.print(); //Expected Result: LinkedList{1,3};
 
-
 let linkedlist2 = new LinkedList();
 linkedlist2.add(4);
 linkedlist2.add(5);
@@ -106,5 +114,6 @@ linkedlist2.print(); //Expected Result: LinkedList{1,2,3};
 linkedlist2.remove(5);
 linkedlist2.print(); //Expected Result: LinkedList{1,3};
 
+console.log("//---Question4---//")
 
-//---Question2---//
+//---Question4---//
