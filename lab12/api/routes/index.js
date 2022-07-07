4ea45b4322f4be2d4362b2788993b6db2029eac6
Sudheesan <surendransudheesan@gmail.com>
2022-07-07 10:28:54 -0500

    Update index.js
    changed resource name

--- lab12/api/routes/index.js
+++ lab12/api/routes/index.js
@@ -5,8 +5,8 @@ const userRouter =  require('./userRoute');
 
 const baseRouter = express.Router();
 
-baseRouter.use('/user', userRouter);
-baseRouter.use('/product', productRouter);
+baseRouter.use('/users', userRouter);
+baseRouter.use('/products', productRouter);
 baseRouter.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'..', '..' ,'views', 'index.html'))
 });
